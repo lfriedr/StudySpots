5e8257d169dd4e71cec8846be7bd8a9e87642fd8
Lauren Friedrich <lfriedr@umich.edu>
2025-03-03 08:51:00 -0800

    Fixed syntax errors. Updates to styling and favicon. Synced local with Heroku's latest version.

--- schemas.js
+++ schemas.js
@@ -36,6 +36,6 @@ module.exports.locationSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
     rating: Joi.number().required().min(1).max(5),
-    body: Joi.string().required().escapeHTML()heroku --version
+    body: Joi.string().required().escapeHTML()
   }).required(),
 });
